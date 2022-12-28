a6deed605dbc30b589cc8033364f0522e54db044
samjaberi <samanjaberi.sj@gmail.com>
2022-12-27 21:17:34 -0500

    experimenting with different urls

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ plSchedule.apiKey = '216fc317fce14a3e92c6759cc84f2ceb';
 
 plSchedule.getSchedule = () => {
 
-    fetch('https://proxy-ugwolsldnq-uc.a.run.app/https://api.football-data.org/v4/competitions/PL/matches', {
+    
+    // url = 'https://proxy-ugwolsldnq-uc.a.run.app/https://api.football-data.org/v4/teams/86/matches'
+    // headers = {'X-Auth-Token': plSchedule.apiKey}
+
+    // fetch(url, headers=headers)
+    fetch('https://proxy-ugwolsldnq-uc.a.run.app/https://api.football-data.org/v4/competitions/WC', {
         method: 'GET',
         headers: {
             'X-Auth-Token': '216fc317fce14a3e92c6759cc84f2ceb',
-        },
-        competitions: '2021',
-        dateFrom: '2022-01-01',
-        DateTo: '2022-10-31',
-        mode: 'cors',
-        cache: 'default',
+        }, 
+        // status: 'SCHEDULED'
     })
         .then((res) => {
             return res.json();
