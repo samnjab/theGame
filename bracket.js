b76a8e9850dc8b9aaa665d36973823c8e2648192
samjaberi <samanjaberi.sj@gmail.com>
2023-01-12 17:01:51 -0500

    the bracket page is now LIVE

--- bracket.js
+++ bracket.js
@@ -3,6 +3,76 @@ const footballStats = {};
 // initialize apikey
 footballStats.apikey = '216fc317fce14a3e92c6759cc84f2ceb';
 
+footballStats.display = (dates, sortedMatches, stage) => {
+    return new Promise((resolve, reject) => {
+    
+        const matchTemplate = document.querySelector("[data-match-template]")
+        const matchContainer = document.querySelector(`.${stage}`)
+        const matchesWithElements = []
+
+        for (let i=0; i<dates.length; i++){
+            
+            const dateElement = matchTemplate.content.cloneNode(true).children[0]
+            const matchHeader = dateElement.querySelector("[data-match-header]")
+            matchHeader.textContent = dates[i].substring(4,10)
+            matchContainer.append(dateElement)
+            
+            const matchTable = dateElement.querySelector("[data-match-Table]")
+            
+            matchesWithElements[i] = sortedMatches[dates[i]].map(match => {
+                const matchBoxTemplate = document.querySelector('[data-match-box]')
+                const matchDiv = matchBoxTemplate.content.cloneNode(true).children[0]
+
+                // <<<<<< Team 1 flag + information starts here >>>>>>>>>
+   
+               const matchTeam1FlagImg = matchDiv.querySelector('[data-flag-team1]')
+               matchTeam1FlagImg.src = match.team1.flag 
+               const matchTeam1Info = matchDiv.querySelector('[data-team1-info]')
+               matchTeam1Info.textContent = `${match.team1.name} ${match.team1.score.fullTime}`
+   
+               // <<<<<< Team 2 flag + information starts here >>>>>>>>>
+   
+               const matchTeam2FlagImg = matchDiv.querySelector('[data-flag-team2]')
+               matchTeam2FlagImg.src = match.team2.flag
+               const matchTeam2Info = matchDiv.querySelector('[data-team2-info]')
+               matchTeam2Info.textContent = `${match.team2.name} ${match.team2.score.fullTime}`
+   
+               // <<<<<< Winner >>>>>>>>>>>>>>>
+            //    const winnerDiv = matchDiv.querySelector('[data-winner]')
+            //    winnerDiv.textContent = `Winner : ${match.winner}`
+               // <<<<<<<<<<< More Info addition >>>>>>>>
+            //    matchDiv.querySelector('[data-competition-name]').textContent = `${match.competition.name}`
+            //    matchDiv.querySelector('[data-competition-emblem]').src = `${match.competition.emblem}`
+            //    matchDiv.querySelector('[data-match-date]').textContent = `Date: ${match.date}`
+            //    matchDiv.querySelector('[data-group]').textContent = `Group: ${match.group}`
+            //    matchDiv.querySelector('[data-stage]').textContent = `Stage: ${match.stage}`
+            //    matchDiv.querySelector('[data-match-day]').textContent = `Match day:${match.matchDay}`
+            //    matchDiv.querySelector('[data-status]').textContent = `Status: ${match.status}`
+            //    matchDiv.querySelector('[data-winner]').textContent = `Winner: ${match.winner}`
+               
+
+
+
+        
+               // <<<<<<<< append >>>>>>>>>>>>>>
+               matchTable.append(matchDiv)
+               
+
+               return {match:match, element:matchDiv}
+            })
+
+
+        }
+        if (matchesWithElements){
+            console.log('we got resolve')
+            resolve(matchesWithElements)
+        }else{
+            reject('data could not be loaded')
+        }
+    }) 
+    
+}
+
 footballStats.convertDate = (utcDate) => {
     date = new Date(utcDate)
     return date.toDateString()
@@ -111,13 +181,16 @@ footballStats.init = () =>{
             console.log('stage is:', stages[i])
             // console.log('sorted matches for stage', stages[i],footballStats.sortByDate(uniqueDatesOfStages[stages[i]], matches[stages[i]]))
             footballStats.sortedMatchesforStages[stages[i]] = footballStats.sortByDate(uniqueDatesOfStages[stages[i]], matches[stages[i]])
-    
             console.log('sorted matches for', stages[i] , footballStats.sortedMatchesforStages[stages[i]])
         }
-        console.log('sorted matches:',sortedMatches)
 
+        stagesClassNames = ['group-stage', 'round16', 'quarter-finals', 'semi-finals','final']
+        stagesClassNames.forEach((stageClassName,i) => {
+            footballStats.display(uniqueDatesOfStages[stages[i]], footballStats.sortedMatchesforStages[stages[i]], stageClassName)
+        })
 
     }
+
     nextStep(stages)
         
 
